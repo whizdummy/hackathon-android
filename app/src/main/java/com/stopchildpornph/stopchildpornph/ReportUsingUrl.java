241995d7701df12501f23bbc4ef3d3ed6218cfbd
kenlayug <ken_layug@yahoo.com>
2016-02-18 13:49:44 +0800

    added report by url.

---
+++ app/src/main/java/com/stopchildpornph/stopchildpornph/ReportUsingUrl.java
@@ -0,0 +1,13 @@
+package com.stopchildpornph.stopchildpornph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportUsingUrl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_using_url);
+    }
+}
