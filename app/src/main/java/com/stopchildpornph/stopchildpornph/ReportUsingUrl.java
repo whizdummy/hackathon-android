52c9df9ec8a1b02fc782769e5c1058e3a3a83ebd
whizdummy <jeraldjohnpormon@yahoo.com>
2016-02-19 01:10:42 +0800

    Labyu

--- app/src/main/java/com/stopchildpornph/stopchildpornph/ReportUsingUrl.java
+++ app/src/main/java/com/stopchildpornph/stopchildpornph/ReportUsingUrl.java
@@ -1,13 +1,30 @@
 package com.stopchildpornph.stopchildpornph;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ReportUsingUrl extends AppCompatActivity {
+    EditText editTxtName;
+    private static final String REPORT_URL = "http://hk4.dswd.gov.ph/test-connection";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_using_url);
+    }
+
+    public void onClick(View v) {
+        editTxtName = (EditText) findViewById(R.id.editTxtName);
+    }
+
+    class Background extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
     }
 }
